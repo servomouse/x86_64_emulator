f859289d919c3fbc9adf06094ccc4a0c50179fec
Servomouse <servomouse@protonmail.com>
2024-11-21 20:07:46 -0600

    Start implementing targets support

--- build.py
+++ build.py
@@ -2,11 +2,13 @@ import subprocess
 import os
 import sys
 import glob
+import toml     # pip install toml
+
 # gcc -shared -o bin/8259a_int_controller.dll devices/8259a_interrupt_controller.c utils.c log_server_iface/logs_win.c -lws2_32 -I. -I./devices
 # gcc -shared -o bin/8259a_int_controller.dll devices/8259a_interrupt_controller.c utils.c -I. -I./devices -Wall
 # gcc -shared -o bin/8086_io.dll 8086_io.c utils.c -I. -I./devices -Wall
 # gcc -shared -o bin/8086_mem.dll 8086_mem.c utils.c -I. -I./devices -Wall
-# gcc -shared -o bin/8086_cpu.dll 8086.c utils.c -I. -I./devices -Wall
+# gcc -shared -o bin/8086_cpu.dll 8086.c utils.c -I. -I./devices -I./tests -Wall
 
 files_to_build = ["main.c",
                   "8086.c",
@@ -19,7 +21,23 @@ files_to_build = ["main.c",
                   "utils.c",
                   "log_server_iface/logs_win.c"]
 output_file_name = "main"
-flags = ["-lm", "-g", "-Wall", "-lws2_32", "-I.", "-I./devices"]
+flags = ["-lm", "-g", "-Wall", "-lws2_32", "-I.", "-I./devices", "-I./tests"]
+
+
+def get_targets(filename: str):
+    with open(filename, 'r') as f:
+        targets = toml.load(f)
+    return targets
+
+
+def build(target):
+    targets = get_targets("targets.toml")
+    if target == "all":
+        pass
+    else:
+        if target in targets and len(targets[target]['module'][0]) > 0:
+            pass
+
 
 
 def build_all():
@@ -47,4 +65,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    print(get_targets("targets.toml"))
+    # main()
