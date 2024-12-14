1c2911d66099967666ebe455c1f549d2aa49380f
Servomouse <servomouse@protonmail.com>
2024-12-14 07:04:57 -0600

    Fix wires

--- pins.h
+++ pins.h
@@ -16,15 +16,13 @@ typedef struct pin_t {
     uint8_t state;
     pin_type_t pin_type;
     uint8_t(*get_state)(void);
-    uint8_t(*wire_get_state)(void);
     void(*_set_value)(uint8_t);     // Directly sets local value
     void(*set_state)(uint8_t);
-    void(*wire_set_state)(uint8_t);
-    void(*wire_state_change_cb)(uint8_t);  // User defined
+    void(*state_change_cb)(uint8_t);  // User defined
 } pin_t;
 
 // Create pin with custom callback
-#define CREATE_PIN_3(_pin_name, _pin_type, _cb) \
+#define CREATE_PIN_MAIN(_pin_name, _pin_type, _cb) \
 void _pin_name##_set_state(uint8_t new_state);  \
 void _pin_name##_set_value(uint8_t new_state);  \
 uint8_t _pin_name##_get_state(void);            \
@@ -33,69 +31,26 @@ pin_t _pin_name = {                             \
     .state = 0,                                 \
     .pin_type = _pin_type,                      \
     .get_state = _pin_name##_get_state,         \
-    .wire_get_state = NULL,                     \
     ._set_value = _pin_name##_set_value,        \
     .set_state = _pin_name##_set_state,         \
-    .wire_set_state = NULL,                     \
-    .wire_state_change_cb = _cb                 \
+    .state_change_cb = _cb                 \
 };                                              \
 void _pin_name##_set_value(uint8_t new_state) { \
     mylog(0, DEVICE_LOG_FILE, "%lld, %s " #_pin_name "_set_value(%d)\n", ticks_num, DEVICE_NAME, new_state); \
     _pin_name.state = new_state;                \
+    _pin_name.state_change_cb(new_state);  \
 }                                               \
 void _pin_name##_set_state(uint8_t new_state) { \
     mylog(0, DEVICE_LOG_FILE, "%lld, %s " #_pin_name "_set_state(%d)\n", ticks_num, DEVICE_NAME, new_state); \
-    if(_pin_name.wire_set_state) {              \
-        _pin_name.wire_set_state(new_state);    \
-    } else {                                    \
-        _pin_name.state = new_state;            \
-    }                                           \
+    _pin_name._set_value(new_state);            \
 }                                               \
 uint8_t _pin_name##_get_state(void) {           \
     mylog(0, DEVICE_LOG_FILE, "%lld, %s " #_pin_name "_get_state()->%d\n", ticks_num, DEVICE_NAME, _pin_name.state); \
-    if(_pin_name.wire_get_state) {              \
-        return _pin_name.wire_get_state();      \
-    } else {                                    \
-        return _pin_name.state;                 \
-    }                                           \
+    return _pin_name.state;                     \
 }
 
-// Create pin with default (dummy) callback
-#define CREATE_PIN_2(_pin_name, _pin_type)      \
-void _pin_name##_set_state(uint8_t new_state);  \
-void _pin_name##_set_value(uint8_t new_state);  \
-uint8_t _pin_name##_get_state(void);            \
-DLL_PREFIX                                      \
-pin_t _pin_name = {                             \
-    .state = 0,                                 \
-    .pin_type = _pin_type,                      \
-    .get_state = _pin_name##_get_state,         \
-    .wire_get_state = NULL,                     \
-    ._set_value = _pin_name##_set_value,        \
-    .set_state = _pin_name##_set_state,         \
-    .wire_set_state = NULL,                     \
-    .wire_state_change_cb = dummy_cb            \
-};                                              \
-void _pin_name##_set_value(uint8_t new_state) { \
-    mylog(0, DEVICE_LOG_FILE, "%lld, %s " #_pin_name "_set_value(%d)\n", ticks_num, DEVICE_NAME, new_state); \
-    _pin_name.state = new_state;                \
-}                                               \
-void _pin_name##_set_state(uint8_t new_state) { \
-    mylog(0, DEVICE_LOG_FILE, "%lld, %s " #_pin_name "_set_state(%d)\n", ticks_num, DEVICE_NAME, new_state); \
-    if(_pin_name.wire_set_state) {              \
-        _pin_name.wire_set_state(new_state);    \
-    } else {                                    \
-        _pin_name.state = new_state;            \
-    }                                           \
-}                                               \
-uint8_t _pin_name##_get_state(void) {           \
-    mylog(0, DEVICE_LOG_FILE, "%lld, %s " #_pin_name "_get_state()->%d\n", ticks_num, DEVICE_NAME, _pin_name.state); \
-    if(_pin_name.wire_get_state) {              \
-        return _pin_name.wire_get_state();      \
-    } else {                                    \
-        return _pin_name.state;                 \
-    }                                           \
-}
+#define CREATE_PIN_3(_pin_name, _pin_type, _cb) CREATE_PIN_MAIN(_pin_name, _pin_type, _cb)  // Create pin with custom callback
+#define CREATE_PIN_2(_pin_name, _pin_type) CREATE_PIN_MAIN(_pin_name, _pin_type, dummy_cb)  // Create pin with default (dummy) callback
 
 // https://gustedt.wordpress.com/2010/06/03/default-arguments-for-c99/
 #define _GET_ARG_COUNT(_0, _1, _2, _3, ...) _3
--- wires.py
+++ wires.py
@@ -18,14 +18,13 @@ class Wire:
     def set_state(self, new_state):
         if self.state == new_state:
             return
-        print(f"{self.name}: setting new state to {new_state}")
-        self.state = new_state
+        print(f"{self.name}: changing state to {new_state}")
         for conn in self.connections:
             print(f"Checking {conn['device_name']}.{conn['pin_name']} state")
             if new_state != conn['struct'].get_state():
                 print(f"Setting {conn['device_name']}.{conn['pin_name']} to {new_state}")
                 conn['struct']._set_value(new_state)
-                conn["struct"].wire_state_change_cb(new_state)
+        self.state = new_state
     
     def connect_device(self, device, pin_name, dev_name):
         @ctypes.CFUNCTYPE(ctypes.c_uint8)
@@ -36,34 +35,17 @@ class Wire:
         def _wire_set_state(new_state):
             self.set_state(new_state)
 
-        # typedef struct pin_t {
-        #     uint8_t state;
-        #     pin_type_t pin_type;
-        #     uint8_t(*get_state)(void);
-        #     uint8_t(*wire_get_state)(void);
-        #     void(*set_state)(uint8_t);
-        #     void(*wire_set_state)(uint8_t);
-        #     void(*wire_state_change_cb)(uint8_t);  // User defined
-        # } pin_t;
-
         class WireStruct(ctypes.Structure):
             _fields_ = [
                 ('state', ctypes.c_uint8),
                 ("pin_type", WireType),
                 ("get_state", ctypes.CFUNCTYPE(ctypes.c_uint8)),
-                ("wire_get_state", ctypes.CFUNCTYPE(ctypes.c_uint8)),
                 ("_set_value", ctypes.CFUNCTYPE(None, ctypes.c_uint8)),
                 ("set_state", ctypes.CFUNCTYPE(None, ctypes.c_uint8)),
-                ("wire_set_state", ctypes.CFUNCTYPE(None, ctypes.c_uint8)),
-                ("wire_state_change_cb", ctypes.CFUNCTYPE(None, ctypes.c_uint8))
+                ("state_change_cb", ctypes.CFUNCTYPE(None, ctypes.c_uint8))
             ]
 
         wire_struct = WireStruct.in_dll(device, pin_name)
-        wire_struct.wire_get_state = _wire_get_state
-        wire_struct.wire_set_state = _wire_set_state
-        pin = {
-            "device_name": dev_name,
-            "pin_name": pin_name,
-            "struct": wire_struct
-        }
-        self.connections.append(pin)
+        wire_struct.get_state = _wire_get_state     # Replace default function
+        wire_struct.set_state = _wire_set_state     # Replace default function
+        self.connections.append({"device_name": dev_name, "pin_name": pin_name, "struct": wire_struct})
